febe4e9da0206b6d546de2c9edaa792cd0bfb6c9
stale.johnsen@outlook.com <stale.johnsen@outlook.com>
2024-03-25 11:50:29 +0100

    Add terraform.tf [skip ci]

---
+++ terraform.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = "~> 1.6"
+  required_providers {
+    azurerm = "~> 3.88"
+  }
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  alias                      = "management"
+  subscription_id            = var.subscription_id_management
+  features {}
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  alias                      = "connectivity"
+  subscription_id            = var.subscription_id_connectivity
+  features {}
+}
+
+provider "azurerm" {
+  alias           = "identity"
+  subscription_id = var.subscription_id_identity
+  features {}
+}
