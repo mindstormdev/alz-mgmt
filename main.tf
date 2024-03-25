a4119d5be2a352e353b94fff17a09f316abb3fb0
stalejohnsen <98233333+stalejohnsen@users.noreply.github.com>
2024-03-25 13:34:41 +0100

    Commented out firewall configuration in main.tf

--- main.tf
+++ main.tf
@@ -33,11 +33,11 @@ module "hubnetworking" {
       address_space       = [var.hub_virtual_network_address_prefix]
       location            = var.default_location
       resource_group_name = "rg-connectivity-${var.default_location}"
-      firewall = {
-        subnet_address_prefix = var.firewall_subnet_address_prefix
-        sku_tier              = "Standard"
-        sku_name              = "AZFW_VNet"
-      }
+      #firewall = {
+      #  subnet_address_prefix = var.firewall_subnet_address_prefix
+      #  sku_tier              = "Standard"
+      #  sku_name              = "AZFW_VNet"
+      #}
     }
   }
 
